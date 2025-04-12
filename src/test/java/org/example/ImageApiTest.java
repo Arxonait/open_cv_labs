5657c7e36844bd25bcf1b84f3694ca55137fda54
nikita <nikk23716@gmail.com>
2025-04-12 21:10:39 +0300

    add test for new feature

--- src/test/java/org/example/ImageApiTest.java
+++ src/test/java/org/example/ImageApiTest.java
@@ -1,10 +1,30 @@
 package org.example;
 
 import org.junit.Test;
+import org.opencv.core.Mat;
 
 public class ImageApiTest {
     @Test
     public void testInit() throws Exception {
         ImageAPI api = new ImageAPI();
     }
+
+    @Test
+    public void testImageApiChannelZeroing() throws Exception {
+        ImageAPI api = new ImageAPI();
+
+        String src = "/home/vboxuser/Pictures/car.jpeg";
+        String dst = "/home/vboxuser/Pictures/car_result.jpeg";
+
+        Mat img = api.loadImage(src);
+        api.showImage(img);
+
+        Mat modified = api.zeroChannel(img, 0); // обнуляем синий канал
+        api.showImage(modified);
+
+        api.saveImage(dst, modified);
+
+        Thread.sleep(10000);
+    }
+
 }
