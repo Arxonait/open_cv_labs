cd70b640291462a556a2a6951581e67377006849
nikita <nikk23716@gmail.com>
2025-05-20 20:31:20 +0300

    add filters

---
+++ src/main/java/org/example/FilterUtils.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.example.FilterResults;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+public class FilterUtils {
+
+    public static FilterResults applyFilters(String imagePath, int kernelSize) {
+        Mat src = Imgcodecs.imread(imagePath);
+        if (src.empty()) {
+            System.err.println("Cannot read image: " + imagePath);
+            return null;
+        }
+
+        Size ksize = new Size(kernelSize, kernelSize);
+
+        // Blur
+        Mat blur = new Mat();
+        Imgproc.blur(src, blur, ksize);
+
+        // Gaussian
+        Mat gaussian = new Mat();
+        Imgproc.GaussianBlur(src, gaussian, ksize, 0);
+
+        // Median
+        Mat median = new Mat();
+        if (kernelSize % 2 == 1 && kernelSize > 1) {
+            Imgproc.medianBlur(src, median, kernelSize);
+        } else {
+            median = src.clone(); // если невалидный размер ядра, вернем копию
+        }
+
+        // Bilateral
+        Mat bilateral = new Mat();
+        Imgproc.bilateralFilter(src, bilateral, kernelSize, 75, 75);
+
+        return new FilterResults(blur, gaussian, median, bilateral);
+    }
+}
